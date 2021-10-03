1d2195b54be0a2e6ccd69443f69d40b07d3e3593
ShutUp-Pls <marcodelgado.1008@gmail.com>
2021-10-03 00:41:15 -0300

    Añadida función "leerDatos"
    La función leerDatos viene a automatizar el proceso de meter los datos del archivo JSON a la variable "data". La idea es que al ingresar los datos a la variable "data" no se tenga que hacer 1 por 1 como lo está en el trozo de codigo comentado.

--- components/Diagrama.js
+++ components/Diagrama.js
@@ -3,42 +3,52 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 const datos = require('./datosTest.json');
 
+const leerDatos = (archivoJson) => {
+  var data = [];
+  for(var i=0; i<archivoJson.length; i++){
+    data.push({name:archivoJson[i].id, pop:archivoJson[i].pop});
+  }
+  return data;
+}
+const data = leerDatos(datos);
 
+/*
 const data = [
   {
     name: datos[0].id,
-    pop: datos[0].pop,
+    pop: datos[0].pop
   },
   {
     name: datos[1].id,
-    pop: datos[1].pop,
+    pop: datos[1].pop
   },
   {
     name: datos[2].id,
-    pop: datos[2].pop,
+    pop: datos[2].pop
   },
   {
     name: datos[3].id,
-    pop: datos[3].pop,
+    pop: datos[3].pop
   },
   {
     name: datos[4].id,
-    pop: datos[4].pop,
+    pop: datos[4].pop
   },
   {
     name: datos[5].id,
-    pop: datos[5].pop,
+    pop: datos[5].pop
   },
   {
     name: datos[6].id,
-    pop: datos[6].pop,
+    pop: datos[6].pop
   },
-];
+]
+*/
 
 class Diagrama extends Component {
   render() {
     return (
-      <ResponsiveContainer width="100%" aspect={3}>
+      <ResponsiveContainer width="50%" aspect={3}>
         <LineChart
           width={500}
           height={300}
