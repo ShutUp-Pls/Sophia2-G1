4fefb836a498f43ac4846c3e78a424973bf7a0a5
@matias_fuentes2 <zhema20141313@gmail.com>
2021-11-12 18:31:56 -0300

    ApiCall
    hay otro campo de input abajito donde se hacen llamadas a la api (Gabriel Boric / Sebastian Sichel son los unicos en la api)

---
+++ components/ApiCall.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+
+const ApiCall = () => {
+  const [fetchName, setFetchName] = useState({})
+  const [name, setName] = useState()
+  let verification = Object.values(fetchName)
+  return (
+    <div>
+
+      <Formik
+        initialValues={{ search: '' }}
+        onSubmit={async values => {
+          let name = values.search
+          setName(name)
+          let names = name.split(" ")
+          const res = await fetch(
+            `http://45.79.169.216:86/persons/?name=${names[0]}%20${names[1]}&page=1&size=50`,
+            {
+              method: "GET",
+              headers: {
+                Authentication: "password",
+                'X-Api-Key': 'password',
+                // update with your user-agent
+                "User-Agent":
+                  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+                Accept: "application/json; charset=UTF-8",
+              },
+            }
+          )
+          const data = await res.json()
+          setFetchName(data.items)
+        }}
+      >
+        <Form>
+          <Field name='search' />
+        </Form>
+      </Formik>
+      {verification["length"] === 0 ? <h2>{name} no existe en la api</h2> : <h2>{name} existe en la api</h2>}
+    </div>
+  );
+}
+
+export default ApiCall
--- components/Input.js
+++ components/Input.js
@@ -2,29 +2,30 @@ import { useState } from 'react';
 
 import Diagrama from './Diagrama';
 import SearchField from 'react-search-field'
+import ApiCall from './ApiCall';
 
-function Input(){
+function Input() {
     const datosJson = require('../public/data/datosTest.json');
     const [datos, setDatos] = useState([{}]);
     const [textoFeedBack, setTextoFeedBack] = useState('Esperando busqueda...');
 
     const convierteDatos = (diccionario, tipo) => { //tipo=0 -> Muestra Menciones, tipo=1 -> Mientra Popularidad, por defecto tipo=1
         var datosFormateados = [];
-        if(tipo == 0){
+        if (tipo == 0) {
             const listaLlaves = Object.keys(diccionario.mentions);
             const listaValores = Object.values(diccionario.mentions);
-            for(var i=0; i<listaLlaves.length; i++){
-                datosFormateados.push({fecha:listaLlaves[i], menciones:listaValores[i]});
+            for (var i = 0; i < listaLlaves.length; i++) {
+                datosFormateados.push({ fecha: listaLlaves[i], menciones: listaValores[i] });
             }
         }
-        else if(tipo == 1){
+        else if (tipo == 1) {
             const listaLlaves = Object.keys(diccionario.popularity);
             const listaValores = Object.values(diccionario.popularity);
-            for(var i=0; i<listaLlaves.length; i++){
-                datosFormateados.push({fecha:listaLlaves[i], popularidad:listaValores[i]});
+            for (var i = 0; i < listaLlaves.length; i++) {
+                datosFormateados.push({ fecha: listaLlaves[i], popularidad: listaValores[i] });
             }
         }
-        else{
+        else {
             datosFormateados = convierteDatos(diccionario, 1);
         }
         return datosFormateados;
@@ -35,27 +36,28 @@ function Input(){
         if (data) {
             const datosFormateados = convierteDatos(data)
             setDatos(datosFormateados);
-            setTextoFeedBack('Mostrado \'' + Object.keys(datosFormateados[0])[1] + '\' de \'' + texto + '\' en pantalla' );
+            setTextoFeedBack('Mostrado \'' + Object.keys(datosFormateados[0])[1] + '\' de \'' + texto + '\' en pantalla');
         }
-        else{
+        else {
             setTextoFeedBack('\'' + texto + '\' no se encuentra en nuestra base de datos');
         }
     }
 
-    return(
+    return (
         <div>
             <SearchField
-            classNames='Searcher'
-            placeholder='Buscar...'
-            onEnter={buscarNombre}
-            onSearchClick={buscarNombre}
+                classNames='Searcher'
+                placeholder='Buscar...'
+                onEnter={buscarNombre}
+                onSearchClick={buscarNombre}
             />
             <p>{textoFeedBack}</p>
             <Diagrama
-            datos={datos}
-            datoEnX={Object.keys(datos[0])[0]}
-            nombreLinea={Object.keys(datos[0])[1]}
+                datos={datos}
+                datoEnX={Object.keys(datos[0])[0]}
+                nombreLinea={Object.keys(datos[0])[1]}
             />
+            <ApiCall />
         </div>
     );
 }
